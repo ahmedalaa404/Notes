60cdd333d98805b553163319f94670ddf494c9cc
ahmedalaa404 <93731317+ahmedalaa404@users.noreply.github.com>
2023-01-20 04:05:12 +0200

    replace index file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = 
-[
+[  
+  {path:"" ,redirectTo:'register',pathMatch:'full'},
   {path:'login',component:LoginComponent,},
   {path:'register',component:RegisterComponent},
   {path:'home',component:HomeComponent,canActivate:[AuthGuard]},
-  {path:"" ,component:RegisterComponent},
+  {path:"**" ,redirectTo:'register',pathMatch:'full'},
 
 ];
 
